81b48f9b9b5953029ffd2edc01e17a6645781268
Bedrauk <111381113+Bedrauk@users.noreply.github.com>
2024-11-01 21:00:51 +0300

    feat: add sound bar

--- player.js
+++ player.js
@@ -8,18 +8,21 @@ const tracks = [
     {
         audio: './assets/mp3/nizkiz-povorachivajj.mp3',
         image: './assets/img/nizkiz.jpg',
-        description: 'Chop Suey — одна из самых известных песен System of a Down.',
+        description: 'Трек вышедшего в 2015 году альбома "Лирика", посвящен герою Беларуси',
     },
     {
         audio: './assets/mp3/Creepy Nuts - Bling-Bang-Bang-Born.mp3',
         image: './assets/img/nuts.webp',
-        description: 'Toxicity — заглавный трек альбома Toxicity, вышедшего в 2001 году.',
+        description: 'Сингл вышедший в Январе 2025 года. Ставшая популярной благадаря аниме Mashle: Magic and Muscles',
     }
 ];
 
 const audioElement = document.getElementById('audio');
 const artistPhoto = document.getElementById('artist-photo');
 const trackDescription = document.getElementById('track-description');
+const playPauseButton = document.getElementById('play-pause-btn');
+const timeline = document.getElementById('timeline');
+const volumeControl = document.getElementById('volume-control');
 
 document.querySelectorAll('.track').forEach(button => {
     button.addEventListener('click', () => {
@@ -30,8 +33,34 @@ document.querySelectorAll('.track').forEach(button => {
         audioElement.play();
 
         artistPhoto.src = selectedTrack.image;
+        artistPhoto.style.display = 'block';
         
         trackDescription.textContent = selectedTrack.description;
 
     });
+});
+playPauseButton.addEventListener('click', () => {
+    if (audioElement.paused) {
+        audioElement.play();
+        playPauseButton.textContent = 'Pause';
+    } else {
+        audioElement.pause();
+        playPauseButton.textContent = 'Play';
+    }
+});
+
+
+audio.addEventListener('timeupdate', () => {
+    const progress = (audioElement.currentTime / audioElement.duration) * 100;
+    timeline.value = progress || 0; 
+});
+
+
+timeline.addEventListener('input', () => {
+    const seekTime = (timeline.value / 100) * audioElement.duration;
+    audioElement.currentTime = seekTime;
+});
+
+volumeControl.addEventListener('input', () => {
+    audioElement.volume = volumeControl.value;
 });
